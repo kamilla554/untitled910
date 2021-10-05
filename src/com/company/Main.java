28f5103d8e686510c34142a448fe9f3b1f8ea88b
User <khayrullina.k.i@edu.mirea.ru>
2021-10-05 12:12:34 +0300

    готовоujnjdj

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,16 @@ public class Main {
     public static void main(String[] args) {
         Company jej = new Company();
         System.out.println(jej.getIncome());
-        for(int i=0; i<180;i++){
-            jej.hire(new Operator("b", "b", 40000, jej));
-            if(i<80) jej.hire(new Manager("b", "b", 60000, jej));
-            if (i<10) jej.hire(new TopManager("a", "a", (int)(Math.random()*20000+80000), jej));
+        Operator a = new Operator("b", "b", 40000, jej);
+        for (int i = 0; i < 180; i++) {
+            jej.hire(a);
+            if (i < 80) jej.hire(new Manager("b", "b", 60000, jej));
+            if (i < 10) jej.hire(new TopManager("a", "a", (int) (Math.random() * 20000 + 80000), jej));
+        }
+        System.out.println(jej.getTopSalaryStaff(10));
+        System.out.println(jej.getLowestSalaryStaff(10));
+        for (int i = 0; i < 135; i++) {
+            jej.fire(a);
         }
         System.out.println(jej.getTopSalaryStaff(10));
         System.out.println(jej.getLowestSalaryStaff(10));
